76944bd291ddf419e7df51f0824448caae3a3499
kunalAnjna <Kunalanjna7747@gmail.com>
2026-04-21 14:18:19 +0530

    jh

--- script.js
+++ script.js
@@ -37,24 +37,20 @@ window.addEventListener("load", () => {
 
 /* ================= TESTIMONIAL SLIDER ================= */
 const data = [
+  
   {
-    img: "event1.jpg",
-    quote: "Connecting classrooms with real-world industry to shape future engineers.\n                                                                           We as a Cosmo Instruments India provided a state-of-the-art leak testing laboratory setup to the students of Government Polytechnic Manesar as part of our CSR initiative, enabling hands-on learning with real-world industrial technologies and equipment.     \n                                                                        Our motto for this initiative was to continuously collaborate with educational institutes to bridge the gap between industry and academia. \n                                                                           The program was accompanied by plantation program to promote sustainability and environmental responsibility. 💐 👏",
+      img: "event2.jpg",
    
+    quote: "We are excited to present our latest innovation designed to redefine precision and reliability in air leak testing.\nThe LS-R701X is built to deliver:\n ✔ Budget-friendly solution without compromising on quality\n✔ High accuracy and consistent performance\n✔ User-friendly operation for seamless testing\n✔ Advanced technology for modern industrial needs\n✔ Versatile applications across multiple industries\n\nAt Cosmo Instruments India, we continue to push the boundaries of quality and innovation, helping industries achieve superior leak testing standards.\n🔧 Whether it’s automotive, electronics, manufacturing, medical appliances — precision matters.\nExplore the next level of leak testing with LS-R701X."
   },
   {
-      img: "event1.jpg",
+      img: "event3.jpg",
    
-    quote: "Connecting classrooms with real-world industry to shape future engineers.\n                                                                           We as a Cosmo Instruments India provided a state-of-the-art leak testing laboratory setup to the students of Government Polytechnic Manesar as part of our CSR initiative, enabling hands-on learning with real-world industrial technologies and equipment.     \n                                                                        Our motto for this initiative was to continuously collaborate with educational institutes to bridge the gap between industry and academia. \n                                                                           The program was accompanied by plantation program to promote sustainability and environmental responsibility. 💐 👏",
+    quote: "On this International Women’s Day, Cosmo Instruments India proudly celebrated the incredible women in our office who contribute their talent, hard work, and passion every day. The celebration was a small way to recognize their achievements and appreciate the positive impact they create in our workplace.\n Let’s continue to support, respect, and empower women—not just today, but every day.",
   },
   {
-      img: "event1.jpg",
-   
-    quote: "Connecting classrooms with real-world industry to shape future engineers.\n                                                                           We as a Cosmo Instruments India provided a state-of-the-art leak testing laboratory setup to the students of Government Polytechnic Manesar as part of our CSR initiative, enabling hands-on learning with real-world industrial technologies and equipment.     \n                                                                        Our motto for this initiative was to continuously collaborate with educational institutes to bridge the gap between industry and academia. \n                                                                           The program was accompanied by plantation program to promote sustainability and environmental responsibility. 💐 👏",
-  },
-  {
-      img: "event1.jpg",
-    quote: "Connecting classrooms with real-world industry to shape future engineers.\n                                                                           We as a Cosmo Instruments India provided a state-of-the-art leak testing laboratory setup to the students of Government Polytechnic Manesar as part of our CSR initiative, enabling hands-on learning with real-world industrial technologies and equipment.     \n                                                                        Our motto for this initiative was to continuously collaborate with educational institutes to bridge the gap between industry and academia. \n                                                                           The program was accompanied by plantation program to promote sustainability and environmental responsibility. 💐 👏",
+      img: "event4.jpg",
+    quote: "Cosmo Instruments India, in collaboration with Inja Foundation, successfully organized a Free Eye Check-Up Camp and Spectacles Distribution Program in Pune.\nThis initiative aimed to promote better eye health by providing free eye examinations and distributing glasses to those in need, helping many individuals improve their vision and quality of life.\nWe are proud to contribute to community welfare and remain committed to supporting initiatives that create a positive and lasting impact on society.\nA heartfelt thank you to all the doctors, volunteers, and participants who made this program a meaningful success",
    
   },
   {
